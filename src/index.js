e34d961dd44d5d480c8cbcfa2c8e97da6b82ad18
amal <amal95salah@gmail.com>
2022-11-07 01:01:35 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+
+  const tasksUl = document.getElementById("tasks");
+
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newTaskDescription = document.getElementById("new-task-description");
+    let taskUl = document.createElement("ul");
+    taskUl.innerText = newTaskDescription.value
+
+    let buttonUl = document.createElement("button");
+    buttonUl.innerText = "x";
+
+    taskUl.appendChild(buttonUl);
+    tasksUl.appendChild(taskUl);
+
+    e.target.reset();
+
+    taskUl.addEventListener("click", (e) => {
+      tasksUl.removeChild(taskUl);
+    });
+  });
+
+
+
 });
